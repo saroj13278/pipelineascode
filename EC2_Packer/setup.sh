690d34b4ddfa3ebce931d1762138bcae76dff656
saroj13278 <saroj.pandarkl@gmail.com>
2024-03-14 19:08:22 +0530

    Create setup.sh

---
+++ EC2_Packer/setup.sh
@@ -0,0 +1,7 @@
+sudo amazon-linux-extras install java-openjdk11
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
